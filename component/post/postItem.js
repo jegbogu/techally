3cd146a395e5806a447486a2abf13f1e5a33dfb1
jegbogu <jegbogu@gmail.com>
2023-09-25 17:30:59 +0100

    optimised

--- component/post/postItem.js
+++ component/post/postItem.js
@@ -1,12 +1,12 @@
 import classes from './postItem.module.css'
 import { useRouter } from 'next/router';
-import { useSession } from 'next-auth/react';
- 
+import Image from 'next/image'
+
 
 
 function PostItem(props) {
   const router = useRouter()
- 
+
   // Split the text into an array of words
   const words = props.description.split(' ');
 
@@ -16,11 +16,11 @@ function PostItem(props) {
   // Join the first 20 words back into a string
   const result = firstTwentyWords.join(' ');
   function showDetailsHandler() {
-  
-      router.push('/post/' + props.id);
-    }
 
-  
+    router.push('/post/' + props.id);
+  }
+
+
 
 
   return (<li className={classes.productItem}>
@@ -28,17 +28,23 @@ function PostItem(props) {
     <div className={classes.item}>
       <div className={classes.figure}>
         <img src={props.image} alt={props.title} />
+        <Image
+          src= {props.image}
+          alt={props.title}
+          width={500}
+          height={500}
+        />
       </div>
 
       <div className={classes.itemBody}>
         <h3>{props.title}</h3>
         <h6>{props.category}</h6>
-         
+
         <div dangerouslySetInnerHTML={{ __html: result }} />
 
         <button onClick={showDetailsHandler}>Read More</button>
-       
-       
+
+
       </div>
 
     </div>

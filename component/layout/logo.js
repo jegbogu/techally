664106952c6b0ba9e89e4fb40cffd29ddb48843e
jegbogu <jegbogu@gmail.com>
2023-10-09 17:14:53 +0100

    logo optimised

--- component/layout/logo.js
+++ component/layout/logo.js
@@ -1,10 +1,18 @@
 import classes from './logo.module.css'
+import Image from 'next/image';
+import logoTwo from '../../public/logoTwo.png'
 const Logo = () => {
-    return ( 
+    return (
         <div className={classes.logo}>
-            <img src="logoTwo.png" alt="logo" width={150}/>
+            <Image
+                src={logoTwo}
+                alt="email"
+                width={150}
+                
+            />
+            {/* <img src="logoTwo.png" alt="logo" width={150} /> */}
         </div>
-     );
+    );
 }
- 
+
 export default Logo;

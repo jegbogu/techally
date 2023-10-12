9112a484385d1a47795469ab8ca54acd09f8dfc4
jegbogu <jegbogu@gmail.com>
2023-10-12 15:11:49 +0100

    siteicon

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps:{session,...pageProps}, }) {
       <Layout>
         <Head>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <link rel="shortcut icon" href="https://joerally.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlogoTwo.1844c623.png&w=384&q=75" type="image/x-icon"/>
+          <link rel="shortcut icon" href="/siteicon.png" type="image/x-icon"/>
         </Head>
         <Component {...pageProps} />
       </Layout>

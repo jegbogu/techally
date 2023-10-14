f37e63e8b7a9ac018a378457003d319cdd50f3d7
jegbogu <jegbogu@gmail.com>
2023-10-14 08:00:59 +0100

    img opt

--- component/portfolio/sectionOne.js
+++ component/portfolio/sectionOne.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import FacebookIcon from '../icons/facebook'
 import GithubIcon from '../icons/github'
 import LinkedinIcon from '../icons/linkedin'
@@ -15,7 +16,15 @@ function SectionOne() {
             </div>
             <div className={classes.intro}>
                 <div className={classes.figure}>
-                    <img src='/profile.jpg' alt='picture' />
+                    <Image
+                        src='/profile.jpg'
+                        alt="profile"
+                        width={500}
+                        height={300}
+                      
+                    />
+
+                    
                 </div>
                 <div className={classes.firstly}>
                     <div className={classes.welcome}>
@@ -26,36 +35,36 @@ function SectionOne() {
                         <p>Software Engineer & Technical Support Personnel</p>
                     </div>
                     <div className={classes.secondly}>
-                    <p><span>Email</span>  jegbogu@gmail.com</p>
-                    <p><span>Phone</span>  +2348167577935</p>
-                    <p><span>Address</span>  Port Harcourt, Rivers State Nigeria</p>
-                    <p><span>Website</span>  joerally.com</p>
-                    <div className={classes.social}>
-                        <div className={classes.icons}>
-                     
-                       <div>
-                      <Link href='https://github.com/jegbogu/' target='_blank'><GithubIcon/></Link> 
-                       </div>
-                       <div>
-                      <Link href='https://www.linkedin.com/in/egbogu-joseph-13092211a/' target='_blank'><LinkedinIcon/></Link> 
-                       </div>
-                        <div>
-                       <Link href='https://twitter.com/ChineduEgbogu' target='_blank'><TwitterIcon/></Link> 
+                        <p><span>Email</span>  jegbogu@gmail.com</p>
+                        <p><span>Phone</span>  +2348167577935</p>
+                        <p><span>Address</span>  Port Harcourt, Rivers State Nigeria</p>
+                        <p><span>Website</span>  joerally.com</p>
+                        <div className={classes.social}>
+                            <div className={classes.icons}>
+
+                                <div>
+                                    <Link href='https://github.com/jegbogu/' target='_blank'><GithubIcon /></Link>
+                                </div>
+                                <div>
+                                    <Link href='https://www.linkedin.com/in/egbogu-joseph-13092211a/' target='_blank'><LinkedinIcon /></Link>
+                                </div>
+                                <div>
+                                    <Link href='https://twitter.com/ChineduEgbogu' target='_blank'><TwitterIcon /></Link>
+                                </div>
+                                <div>
+                                    <Link href='https://www.youtube.com/channel/UCgarg3Tr-WtAhKCesxJonJA' target='_blank'><YoutubeIcon /></Link>
+                                </div>
+                                <div>
+                                    <Link href='https://drive.google.com/file/d/1iZdEUEDcMxcMRPBrfzZ9s1W26-Es9ahm/view' target='_blank'><ResumeIcon /></Link>
+                                </div>
+
+                            </div>
+
                         </div>
-                        <div>
-                       <Link href='https://www.youtube.com/channel/UCgarg3Tr-WtAhKCesxJonJA' target='_blank'><YoutubeIcon/></Link> 
-                        </div>
-                        <div>
-                      <Link href='https://drive.google.com/file/d/1iZdEUEDcMxcMRPBrfzZ9s1W26-Es9ahm/view' target='_blank'><ResumeIcon/></Link>  
-                        </div>
-                      
-                        </div>
-                       
+                    </div>
                 </div>
-                </div>
-                </div>
-                
-                
+
+
 
             </div>
 
--- component/project/project.js
+++ component/project/project.js
@@ -2,45 +2,53 @@ import Link from 'next/link'
 import data from './data'
 import classes from './project.module.css'
 import { useState } from 'react'
-
+import Image from 'next/image'
 function Project() {
-const[display, setDisplay] = useState(classes.hide)
-const [picture,setPicture] = useState(" ")    
-const [title,setTitle] = useState(false)    
+    const [display, setDisplay] = useState(classes.hide)
+    const [picture, setPicture] = useState(" ")
+    const [title, setTitle] = useState(false)
 
 
 
     return (
-        
 
-        
+
+
         <div className={classes.allPro}>
 
-            {data.map((el,i) => {
+            {data.map((el, i) => {
                 return (
                     <div className={classes.projects} key={i}>
 
                         <div className={classes.proF}>
                             <div className={classes.figure}>
-                                <img src={el.banner} alt='project'/>
+                                <Image
+                                    src={el.banner}
+                                    alt="project"
+                                    width={200}
+                                    height={150}
+
+                                />
+                                
                             </div>
-                            <button onClick={()=>{
-                             const s =  data.find((e)=>{
-                                return e.title === el.title
-                               })
-                              setPicture(s.picture)
-                              setTitle(s.title)
-                              setDisplay(classes.show)
+                            <button onClick={() => {
+                                const s = data.find((e) => {
+                                    return e.title === el.title
+                                })
+                                setPicture(s.picture)
+                                setTitle(s.title)
+                                setDisplay(classes.show)
                             }}>View </button>
                             <div className={display}>
-                            <div className={classes.picture}>
-                                <button onClick={()=>{
-                                    setDisplay(classes.hide)
-                                }}>Close Modal</button>
-                                <img src={picture} alt='project'/>
+                                <div className={classes.picture}>
+                                    <button onClick={() => {
+                                        setDisplay(classes.hide)
+                                    }}>Close Modal</button>
+
+                                    <img src={picture} alt='project' />
+                                </div>
                             </div>
-                            </div>
-                            
+
                             <div className={classes.title}>
                                 <p>{el.title}</p>
                             </div>
@@ -56,7 +64,7 @@ const [title,setTitle] = useState(false)
                 )
             })}
         </div>
-  
+
     )
 }
 
